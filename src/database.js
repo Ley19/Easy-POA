5027704a4e0e742eedee243549a0ddda16c1dd78
JaredPV <jaredpv99@micorreo.upp.edu.mx>
2022-11-13 16:08:33 -0600

    Conexión sql

--- src/database.js
+++ src/database.js
@@ -6,3 +6,20 @@ mongoose.connect('mongodb://localhost/Imagenes',{
 
     .then(db=> console.log('DB conectada'))
     .catch(err=>console.error(err))
+
+const mysql = require('mysql');
+var mysqlConnection = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "",
+    database : "easypoa",
+    multipleStatements : true
+ })
+
+ mysqlConnection.connect((err)=>{
+    if(!err){
+        console.log("Conectado a la base de datos")
+    }else{
+        console.log("Conexión a la base de datos fallida")
+    }
+ })
