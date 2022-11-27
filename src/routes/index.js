98f6ec2d97cc1d2cc3df3fe9c81ba5c1afc808ef
JaredPV <jaredpv99@micorreo.upp.edu.mx>
2022-11-27 08:14:36 -0600

    Inicio creacion de pdf

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const {Router, response}=require ('express');
 const router =Router();
-
+const {PDFDocument} = require('pdf-lib')
+const {readFile, writeFile} = require('fs/promises')
+const fontkit = require('@pdf-lib/fontkit')
 const Articulo=require('../models/articulo');
 var database = require('../database');
 
@@ -29,6 +31,8 @@ router.get('/infoadicional',(req,res) =>{
     console.log(req.file);
     res.render('infoadicional');
 });
+
+// RUTAS PARA REQUISICION
 router.get('/requisicion',function(req,res,next){
     database.query('SELECT * FROM actividad ORDER BY idActividad ASC', function(err,data){
         data = JSON.parse(JSON.stringify(data))
@@ -40,7 +44,7 @@ router.get('/requisicion',function(req,res,next){
 router.get('/get_calendario', function(req,res){
     var idActividad = req.query.idActividad
     
-    database.query('SELECT * FROM calendario WHERE idActividad ='+idActividad+' ORDER BY idPartida ASC', function(err,data){
+    database.query('SELECT * FROM calendario WHERE actividad ='+idActividad+' ORDER BY partida ASC', function(err,data){
         data = JSON.parse(JSON.stringify(data))
         res.json(data);
     })
@@ -53,6 +57,43 @@ router.get('/get_partida', function(req,res){
         res.json(data);
     })
 })
+
+router.get('/get_mes', function(req,res){
+    var idActividad = req.query.idActividad
+    var idPartida = req.query.idPartida
+    console.log(idActividad,idPartida)
+    database.query('SELECT * FROM calendario WHERE actividad='+idActividad+' AND partida='+idPartida, function(err,data){
+        data = JSON.parse(JSON.stringify(data[0]))
+        res.json(data);
+    })
+})
+
+router.post('/get_pdf',function(req,res){
+    async function createPdf (input,output){
+        try{
+            const pdfDoc = await PDFDocument.load(await readFile(input));
+
+            pdfDoc.registerFontkit(fontkit)
+            const montserratFont= await readFile('./temp/Montserrat-Regular.ttf')
+            const montserrat = await pdfDoc.embedFont(montserratFont)
+
+            const fields = pdfDoc.getForm().getFields().map(f=>f.getName())
+            console.log(fields)
+
+            const form = pdfDoc.getForm();
+
+            const pdfBytes = await pdfDoc.save();
+
+            await writeFile(output, pdfBytes);
+            console.log('PDF creado');
+        }catch(err){
+            console.log(err)
+        }
+    }
+    createPdf('./temp/formato_requisicion.pdf','./temp/output.pdf' )
+})
+
+
 
 router.post('/agregarArticulo',async(req,res) =>{
     const articulo = new Articulo();
