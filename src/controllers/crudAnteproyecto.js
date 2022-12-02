f84dc71d20bc9048cadf9a4c0cff812df1f5ba13
Ley19 <santillanleylani19@gmail.com>
2022-12-01 19:45:37 -0600

    CRUD anteproyecto Terminado

--- src/controllers/crudAnteproyecto.js
+++ src/controllers/crudAnteproyecto.js
@@ -1,5 +1,7 @@
-
 const connection = require('../database');
+const date = new Date();
+const PDF = require('pdfkit');
+const fs = require('fs');
 
 //Guardar registro
     exports.saveAnteproyecto = (req, res)=>{
@@ -18,19 +20,17 @@ const connection = require('../database');
         const Noviembre = req.body.Noviembre;
         const Diciembre = req.body.Diciembre;
         const Total = req.body.Total;
-        const Fecha = new Date();
+        const Fecha = date.getFullYear();
 
-        console.log("Resultados: ", Total, " - ", Fecha);
         connection.query('INSERT INTO anteproyecto SET ?',{Actividad:Actividad, Partida:Partida, Enero:Enero, Febrero:Febrero, Marzo:Marzo, Abril:Abril, Mayo:Mayo, Junio:Junio, Julio:Julio, Agosto:Agosto, Septiembre:Septiembre, Octubre:Octubre, Noviembre:Noviembre, Diciembre:Diciembre, Total:Total, Fecha:Fecha}, (error, results)=>{
             if(error){
                 console.log(error);
-            }else{  
-            ;
+            }else{
                 res.redirect('/anteproyecto');         
             }
         });
     };
-//ACTUALIZAR un REGISTRO
+//ACTUALIZAR UN REGISTRO
     exports.updateAnteproyecto = (req, res)=>{
         const id = req.body.id;
         const Actividad = req.body.Actividad;
@@ -48,42 +48,23 @@ const connection = require('../database');
         const Noviembre = req.body.Noviembre;
         const Diciembre = req.body.Diciembre;
         const Total = req.body.Total;
-        const Fecha = req.body.Fecha;
+        const Fecha = date.getFullYear();
         
-        connection.query('UPDATE anteproyecto SET ? WHERE id = ?',[{Actividad:Actividad, Partida:Partida, Enero:Enero, Febrero:Febrero, Marzo:Marzo, Abril:Abril, Mayo:Mayo, Junio:Junio, Julio:Julio, Agosto:Agosto, Septiembre:Septiembre, Octubre:Octubre, Noviembre:Noviembre, Diciembre:Diciembre, Total:Total, Fecha:Fecha}, id], (error, results)=>{
+        connection.query('UPDATE anteproyecto SET ? WHERE id = ?',[{Actividad:Actividad, Partida:Partida, Enero:Enero, Febrero:Febrero, Marzo:Marzo, Abril:Abril, Mayo:Mayo, Junio:Junio, Julio:Julio, Agosto:Agosto, Septiembre:Septiembre, Octubre:Octubre, Noviembre:Noviembre, Diciembre:Diciembre, Total:Total, Fecha:Fecha},id], (error, results)=>{
             if(error){
                 console.log(error);
-            }else{           
+            }else{
                 res.redirect('/anteproyecto');          
             }
          });
     };
 
+//Generar PDF
+    
+    exports.getPDF = async (req, res)=>{
+       const doc = new PDF();
+       doc.text('Hola mundo con pdf kit', 30,30);
 
-    /*
-//Funcion del tiempo
-    let horas = 7;
-    let minutos = 0;
-    let segundos = 0;
-
-    //definimos y ejecutamos los segundos
-
-    function cargaSegundos(){
-        let txtSegundos;
-        if(segundos < 0){
-            segundos = 59;
-        }
-        //Mostrar segundos en pantalla
-        if(segundos < 10){
-            txtSegundos = `0$(segundos)`;
-        }else{
-            txtSegundos = segundos;
-        }
-        document.getElementById('segundos').innerHTML = txtSegundos;
-        segundos --;
-    }
-
-    //Ejecutamos cada segundo
-
-    setInterval(cargaSegundos, 1000)
-    */
+       doc.pipe(fs.createReadStream('anteproyecto.pdf'));
+       doc.end();
+    };
