c3f3aeaaab8d2ad206645b7654938509550a1ce2
MTKArmandoGP <jsearmando767@micorreo.upp.edu.mx>
2023-04-05 23:45:42 -0600

    Activos e inactivos, cambio de la vista de consulta y cambio en agregar articulo

--- src/models/articulo.js
+++ src/models/articulo.js
@@ -11,18 +11,18 @@ const articuloSchema = new Schema({
     costoAdquisicion:{type:Number},
     numFactura:{type:Number},
     description:{type:String},
-    
-    etiquebi:{type:String},
-    seguim:{type:String},
-    estatusbi:{type:String},
-    bajabien:{type:String},
-    fechabaja:{type:String},
-    registrocon:{type:String},
-    registrodb:{type:String},
-    grupobien:{type:String},
-    trataconta:{type:String},
-    NombreSolici:{type:String},
-    areasolici:{type:String}
+
+    etiquebi:{type:String},//etiqueta del bien
+    seguim:{type:String},//Seguimiento de la etiqueta del bien
+    estatusbi:{type:String},//Estatus del bien
+    bajabien:{type:String},//
+    fechabaja:{type:String},//fecha de la baja del articulo
+    registrocon:{type:String},//aplica para el registro contable
+    registrodb:{type:String},//ESTA REGISTRADO EN LA BASE CONTABLE
+    grupobien:{type:String},//GRUPO DE BIENES (ACORDE A LA CÉDULA DE DEPRECIACIÓN)
+    trataconta:{type:String},//TRATAMIENTO CONTABLE
+    NombreSolici:{type:String},//Solicitante
+    areasolici:{type:String}//area solicitante
     
 
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,89 +1,99 @@
-const {Router, response}=require ('express');
-const router =Router();
+const { Router, response } = require("express");
+const router = Router();
 
 //Importaciones para Inventarios
-const Articulos=require('../models/articulo');
-const resguardos=require('../models/resguardos');
-const infAdicional=require('../models/infoAdicional');
-const mongoose=require('mongoose');
+const Articulos = require("../models/articulo");
+const resguardos = require("../models/resguardos");
+const infAdicional = require("../models/infoAdicional");
+const mongoose = require("mongoose");
 
 //Importaciones para Anteproyecto
-const pdfCtrl=require('../controllers/creacionPdf');
-const database = require('../database');
-const crudAnteproyecto = require('../controllers/crudAnteproyecto');
-const requisicionCtrl = require('../controllers/requisicion');
-const transferir = require('../controllers/transferir');
-const { appendBezierCurve } = require('pdf-lib');
+const pdfCtrl = require("../controllers/creacionPdf");
+const database = require("../database");
+const crudAnteproyecto = require("../controllers/crudAnteproyecto");
+const requisicionCtrl = require("../controllers/requisicion");
+const transferir = require("../controllers/transferir");
+const { appendBezierCurve } = require("pdf-lib");
 
-router.get('/', async (req,res) =>{
-    database.query('SELECT * FROM requisicion WHERE usuario=?',0,(error,results)=>{
-        if(error) throw error
-        results = JSON.parse(JSON.stringify(results))
-        console.log(results);
-        res.render('index',{requisiciones_data:results});
-    })
-    
+router.get("/", async (req, res) => {
+  database.query(
+    "SELECT * FROM requisicion WHERE usuario=?",
+    0,
+    (error, results) => {
+      if (error) throw error;
+      results = JSON.parse(JSON.stringify(results));
+      console.log(results);
+      res.render("index", { requisiciones_data: results });
+    }
+  );
 });
 
-router.get('/calendario',(req,res) =>{
-     database.query('SELECT idActividad, nombre FROM actividad ORDER BY idActividad ASC', function(err,data){
-        data = JSON.parse(JSON.stringify(data))
-        res.render('calendario', {actividad_data: data})
-    });
+router.get("/calendario", (req, res) => {
+  database.query(
+    "SELECT idActividad, nombre FROM actividad ORDER BY idActividad ASC",
+    function (err, data) {
+      data = JSON.parse(JSON.stringify(data));
+      res.render("calendario", { actividad_data: data });
+    }
+  );
 });
-
-
 
 //RUTAS PARA ANTEPROYECTO
-router.get('/crearAnteproyecto',(req,res) =>{
-    console.log(req.file);
-    database.query('SELECT * FROM partida', (error, results)=>{
-        if(error){
-            throw error;
-        }else{
-            res.render('crearAnteproyecto', {results:results});
-        }
-    })
+router.get("/crearAnteproyecto", (req, res) => {
+  console.log(req.file);
+  database.query("SELECT * FROM partida", (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.render("crearAnteproyecto", { results: results });
+    }
+  });
 });
 
-router.get('/editarAnteproyecto/:id',(req, res)=>{
-    const id = req.params.id;
-    database.query("SELECT * FROM anteproyecto WHERE id=? ", [id], (error,results)=>{
-        if (error) {
-            throw error;
-        }else{
-            res.render('editarAnteproyecto', {Partida:results[0]});
-        }
-    });
+router.get("/editarAnteproyecto/:id", (req, res) => {
+  const id = req.params.id;
+  database.query(
+    "SELECT * FROM anteproyecto WHERE id=? ",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      } else {
+        res.render("editarAnteproyecto", { Partida: results[0] });
+      }
+    }
+  );
 });
 
-router.get('/eliminarAnteproyecto/:id', (req, res) => {
-    const id = req.params.id;
-    database.query(" DELETE FROM anteproyecto WHERE id=?", [id], (error,results)=>{
-        if(error){
-            console.log(error);
-        }else{
-            res.redirect('/anteproyecto');
-        }
-    });
+router.get("/eliminarAnteproyecto/:id", (req, res) => {
+  const id = req.params.id;
+  database.query(
+    " DELETE FROM anteproyecto WHERE id=?",
+    [id],
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.redirect("/anteproyecto");
+      }
+    }
+  );
 });
 
-router.get('/anteproyecto',(req,res) =>{
-    console.log(req.file);
+router.get("/anteproyecto", (req, res) => {
+  console.log(req.file);
 
-    database.query('SELECT Partida FROM anteproyecto', (error, results)=>{
-        if (error) {
-            throw error;
-        }else{
-            res.render('anteproyecto', {results:results});
-        }
-    })
-    
+  database.query("SELECT Partida FROM anteproyecto", (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.render("anteproyecto", { results: results });
+    }
+  });
 });
 
-
-router.post('/saveAnteproyecto', crudAnteproyecto.saveAnteproyecto);
-router.post('/updateAnteproyecto', crudAnteproyecto.updateAnteproyecto);
+router.post("/saveAnteproyecto", crudAnteproyecto.saveAnteproyecto);
+router.post("/updateAnteproyecto", crudAnteproyecto.updateAnteproyecto);
 
 //BOTONES DE EDICION
 
@@ -96,210 +106,243 @@ router.post('/updateAnteproyecto', crudAnteproyecto.updateAnteproyecto);
     })
 })*/
 
-
 //RUTAS DE TRANSFERENCIAS
 
-router.get('/transferencias',(req,res) =>{
-    console.log(req.file);
-    database.query('SELECT * FROM anteproyecto', (error, results)=>{
-        if (error) {
-            throw error;
-        }else{
-            res.render('transferencias', {results:results});
-        }
-    })
-    
+router.get("/transferencias", (req, res) => {
+  console.log(req.file);
+  database.query("SELECT * FROM anteproyecto", (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.render("transferencias", { results: results });
+    }
+  });
 });
 
-
-router.post('/saveTransferir', transferir.saveTransferir);
-
-
+router.post("/saveTransferir", transferir.saveTransferir);
 
 // RUTAS PARA REQUISICION
-router.get('/requisicion',function(req,res,next){
-    database.query('SELECT * FROM actividad ORDER BY idActividad ASC', function(err,data){
-        data = JSON.parse(JSON.stringify(data))
-        res.render('requisicion', {actividad_data: data})
-    });
+router.get("/requisicion", function (req, res, next) {
+  database.query(
+    "SELECT * FROM actividad ORDER BY idActividad ASC",
+    function (err, data) {
+      data = JSON.parse(JSON.stringify(data));
+      res.render("requisicion", { actividad_data: data });
+    }
+  );
+});
+router.get("/editar-requisicion/:id", requisicionCtrl.getRequisicion);
+router.get("/get_calendario", function (req, res) {
+  var idActividad = req.query.idActividad;
 
-    
-})
-router.get('/editar-requisicion/:id',requisicionCtrl.getRequisicion);
-router.get('/get_calendario', function(req,res){
-    var idActividad = req.query.idActividad
-    
-    database.query('SELECT * FROM calendario WHERE actividad ='+idActividad+' ORDER BY partida ASC', function(err,data){
-        data = JSON.parse(JSON.stringify(data))
-        res.json(data);
-    })
-})
-
-router.get('/get_partida', function(req,res){
-    var idPartida = req.query.idPartida
-    database.query('SELECT * FROM partida WHERE idPartida ='+idPartida+' LIMIT 1', function(err,data){
-        data = JSON.parse(JSON.stringify(data[0]))
-        res.json(data);
-    })
-})
-
-router.get('/get_mes', function(req,res){
-    var idActividad = req.query.idActividad
-    var idPartida = req.query.idPartida
-    database.query('SELECT * FROM calendario WHERE actividad='+idActividad+' AND partida='+idPartida, function(err,data){
-        data = JSON.parse(JSON.stringify(data[0]))
-        res.json(data);
-    })
-})
-
-router.post('/get_pdf', pdfCtrl.pdfRequisicion);
-
-router.post('/guardarRequisicion', requisicionCtrl.guardarRequisicion)
-
-router.post('/actualizarRequisicion', requisicionCtrl.actualizarRequisicion)
-
-router.get('/image/:id',(req,res) =>{
-    res.send('Perfil de la Imagen');
+  database.query(
+    "SELECT * FROM calendario WHERE actividad =" +
+      idActividad +
+      " ORDER BY partida ASC",
+    function (err, data) {
+      data = JSON.parse(JSON.stringify(data));
+      res.json(data);
+    }
+  );
 });
 
-router.get('/image/:id/delete',(req,res) =>{
-    res.send('Imagen Eliminada');
+router.get("/get_partida", function (req, res) {
+  var idPartida = req.query.idPartida;
+  database.query(
+    "SELECT * FROM partida WHERE idPartida =" + idPartida + " LIMIT 1",
+    function (err, data) {
+      data = JSON.parse(JSON.stringify(data[0]));
+      res.json(data);
+    }
+  );
+});
+
+router.get("/get_mes", function (req, res) {
+  var idActividad = req.query.idActividad;
+  var idPartida = req.query.idPartida;
+  database.query(
+    "SELECT * FROM calendario WHERE actividad=" +
+      idActividad +
+      " AND partida=" +
+      idPartida,
+    function (err, data) {
+      data = JSON.parse(JSON.stringify(data[0]));
+      res.json(data);
+    }
+  );
+});
+
+router.post("/get_pdf", pdfCtrl.pdfRequisicion);
+
+router.post("/guardarRequisicion", requisicionCtrl.guardarRequisicion);
+
+router.post("/actualizarRequisicion", requisicionCtrl.actualizarRequisicion);
+
+router.get("/image/:id", (req, res) => {
+  res.send("Perfil de la Imagen");
+});
+
+router.get("/image/:id/delete", (req, res) => {
+  res.send("Imagen Eliminada");
 });
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< RUTAS DE INVENTARIOS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-    //<<<<<<<<<<<<<<<<< MÉTODOS GET >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<<< MÉTODOS GET >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-    router.get('/agregarArticulo',(req,res) =>{
-        console.log(req.file);
-        res.render('agregarArticulo');
+router.get("/agregarArticulo", (req, res) => {
+  console.log(req.file);
+  res.render("agregarArticulo");
+});
+
+/*router.get("/infoadicional/:id", (req, res) => {
+  const id = mongoose.Types.ObjectId(req.params.id);
+  console.log(id);
+  res.render("infoadicional", { id: id });
+});*/
+
+router.get("/Resguardos", (req, res) => {
+  console.log(req.file);
+  res.render("Resguardos");
+});
+
+router.get("/inventario", async (req, res) => {
+  var articulos = await Articulos.find({ estatusbi: "Activo" }); // filter documents by estatus = 'Activo'
+  articulos = JSON.parse(JSON.stringify(articulos));
+  res.render("inventario", {
+    articulos_data: articulos,
+  });
+});
+
+router.get("/consulta/:id", (req, res) => {
+  const articleId = req.params.id;
+  Articulos.findById(articleId, (err, article) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(article);
+      res.render("consulta", { article: article });
+    }
+  });
+});
+
+//<<<<<<<<<<<<<<< MÉTODOS POST >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+router.post('/inventario/estatus', async (req, res) => {
+    const estatus = req.body.estatus; // obtener el valor del botón presionado
+    
+    let articulos;
+    if (estatus === 'activo') {
+        articulos = await Articulos.find({ estatusbi: 'Activo' }); // filtrar por estatus = 'Activo'
+    } else if (estatus === 'inactivo') {
+        articulos = await Articulos.find({ estatusbi: 'Inactivo' }); // filtrar por estatus = 'Inactivo'
+    }
+    
+    articulos = JSON.parse(JSON.stringify(articulos));
+    res.render('inventario', {
+        articulos_data: articulos
     });
-    
-    router.get('/infoadicional/:id',(req,res) =>{
-        const id = mongoose.Types.ObjectId(req.params.id);;
-        console.log(id);
-        res.render('infoadicional', { id: id });
+});
 
-    });
-    
-    router.get('/Resguardos',(req,res) =>{
-        console.log(req.file);
-        res.render('Resguardos');
-    }); 
+router.post("/agregarArticulo", async (req, res) => {
+  const articulo = new Articulos();
+  articulo.nombre = req.body.nombreBien;
+  articulo.numInventario = req.body.numInventario;
+  articulo.clvControl = req.body.clvControl;
+  articulo.marca = req.body.marca;
+  articulo.modelo = req.body.modelo;
+  articulo.tipoAlta = req.body.tipoAlta;
+  articulo.costoAdquisicion = req.body.costoAdquisicion;
+  articulo.numFactura = req.body.numFactura;
+  articulo.description = req.body.descripcion;
+  articulo.etiquebi=req.body.etibi;
+  articulo.seguim=req.body.segui;
+  articulo.registrocon=req.body.regicon;
+  articulo.registrodb=req.body.regidb;
+  articulo.grupobien=req.body.grubi;
+  articulo.trataconta=req.body.tratacon;
+  articulo.NombreSolici=req.body.NombreSo;
+  articulo.areasolici=req.body.areaso;
+  articulo.estatusbi = "Activo";
+  articulo.bajabien="N/A";
+  //articulo.image=req.file.image;
+  await articulo.save();
+  const nuevoId = articulo._id;
+  console.log(articulo);
+  res.redirect("/inventario");
+});
 
-    router.get('/inventario',async (req,res) =>{
-        //console.log(await Articulos.find())
-        var articulos=await Articulos.find()
-        articulos=JSON.parse(JSON.stringify(articulos))
-        res.render('inventario',{
-            articulos_data:articulos
-        })
-    });
+/*router.post("/infoadicional/:id", async (req, res) => {
+  const id = mongoose.Types.ObjectId(req.params.id);
+  //if (!mongoose.Types.ObjectId.isValid(id)) {
+  //  return res.status(400).send('El ID del artículo es inválido');
+  //}
+  const infoAdicional = {
+    etiquebi: req.body.etibi,
+    seguim: req.body.segui,
+    estatusbi: req.body.estabi,
+    bajabien: req.body.bajabi,
+    fechabaja: req.body.fechaba,
+    registrocon: req.body.regicon,
+    registrodb: req.body.regidb,
+    grupobien: req.body.grubi,
+    trataconta: req.body.tratacon,
+    NombreSolici: req.body.NombreSo,
+    areasolici: req.body.areaso,
+  };
 
-    router.get('/consulta/:id', (req, res) => {
-        const articleId = req.params.id;
-        Articulos.findById(articleId, (err, article) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(article);
-                res.render('consulta', { article: article });
-            }
-        });
-    });
+  try {
+    const result = await Articulos.updateOne(
+      { _id: id },
+      { $set: infoAdicional }
+    );
+    console.log(id);
+    console.log(result);
+    res.redirect("/inventario");
+  } catch (error) {
+    console.error(error);
+    res.send(error);
+  }
 
-    //<<<<<<<<<<<<<<< MÉTODOS POST >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  //const result = await Articulos.updateOne({ _id: id }, { $set: infoAdicional });
 
-    router.post('/agregarArticulo',async(req,res) =>{
-        const articulo = new Articulos();
-        articulo.nombre=req.body.nombreBien;
-        articulo.numInventario=req.body.numInventario;
-        articulo.clvControl= req.body.clvControl;
-        articulo.marca=req.body.marca;
-        articulo.modelo=req.body.modelo;
-        articulo.tipoAlta=req.body.tipoAlta;
-        articulo.costoAdquisicion=req.body.costoAdquisicion;
-        articulo.numFactura=req.body.numFactura;
-        articulo.description=req.body.descripcion;
-        //articulo.image=req.file.image;
-        await articulo.save();
-        const nuevoId = articulo._id;
-        console.log(articulo);
-        res.redirect('/infoadicional/${articulo._id}');
-    });
-    
-    router.post('/infoadicional/:id',async(req,res) =>{
-        const id = mongoose.Types.ObjectId(req.params.id);;
-        //if (!mongoose.Types.ObjectId.isValid(id)) {
-          //  return res.status(400).send('El ID del artículo es inválido');
-        //}
-        const infoAdicional = {
-            etiquebi: req.body.etibi,
-            seguim: req.body.segui,
-            estatusbi: req.body.estabi,
-            bajabien: req.body.bajabi,
-            fechabaja: req.body.fechaba,
-            registrocon: req.body.regicon,
-            registrodb: req.body.regidb,
-            grupobien: req.body.grubi,
-            trataconta: req.body.tratacon,
-            NombreSolici: req.body.NombreSo,
-            areasolici: req.body.areaso
-        };
-        
-        try {
-            const result = await Articulos.updateOne({ _id: id }, { $set: infoAdicional });
-            console.log(id);
-            console.log(result);
-            res.redirect('/inventario');
-          } catch (error) {
-            console.error(error);
-            res.send(error);
-          }
+  //console.log(id);
+  //console.log(result);
+  //res.redirect('/inventario');
+});*/
 
-        //const result = await Articulos.updateOne({ _id: id }, { $set: infoAdicional });
-    
-        //console.log(id);
-        //console.log(result);
-        //res.redirect('/inventario');
-    });
+router.post("/Resguardos", async (req, res) => {
+  const resguardo = new resguardos();
+  resguardo.Resguardo = req.body.Resguardo;
+  resguardo.Tipo = req.body.TipoResguardo;
+  resguardo.FechaEla = req.body.FechaResguardo;
+  resguardo.CEtiqueta = req.body.cuenEtiq;
+  resguardo.Seguimiento = req.body.segEti;
+  resguardo.NomRes = req.body.NombreRes;
+  resguardo.NuResguardo = req.body.NumRes;
+  resguardo.AreaAds = req.body.AreaAdscrip;
+  resguardo.ubicacion = req.body.UbiFisica;
+  resguardo.PerfilAcadem = req.body.PerAcademico;
+  resguardo.Puesto = req.body.puest;
+  resguardo.estatus = req.body.EstatLaboral;
+  resguardo.correoPer = req.body.CorreoPersonal;
+  resguardo.correoIns = req.body.CorrInstitucion;
+  resguardo.firmado = req.body.ResgFirmado;
+  resguardo.añofirmado = req.body.AñoFirma;
+  resguardo.observaciones = req.body.obser;
+  //articulo.image=req.file.image;
+  await resguardo.save();
 
-    router.post('/Resguardos',async(req,res) =>{
-        const resguardo = new resguardos();
-        resguardo.Resguardo=req.body.Resguardo;
-        resguardo.Tipo=req.body.TipoResguardo;
-        resguardo.FechaEla= req.body.FechaResguardo;
-        resguardo.CEtiqueta=req.body.cuenEtiq;
-        resguardo.Seguimiento=req.body.segEti;
-        resguardo.NomRes=req.body.NombreRes;
-        resguardo.NuResguardo=req.body.NumRes;
-        resguardo.AreaAds=req.body.AreaAdscrip;
-        resguardo.ubicacion=req.body.UbiFisica;
-        resguardo.PerfilAcadem=req.body.PerAcademico;
-        resguardo.Puesto=req.body.puest;
-        resguardo.estatus=req.body.EstatLaboral;
-        resguardo.correoPer=req.body.CorreoPersonal;
-        resguardo.correoIns=req.body.CorrInstitucion;
-        resguardo.firmado=req.body.ResgFirmado;
-        resguardo.añofirmado=req.body.AñoFirma;
-        resguardo.observaciones=req.body.obser;
-        //articulo.image=req.file.image;
-        await resguardo.save();
-        
-        
-        console.log(req.file);
-        res.redirect('/inventario');
-        });
+  console.log(req.file);
+  res.redirect("/inventario");
+});
 
-    //<<<<<<<<<<<<<<<< MÉTODOS DELETE >>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<< MÉTODOS DELETE >>>>>>>>>>>>>>>>>>>>>
 
-    router.delete('/borrarArticulo',(req, res) => {
-        db.articulos.remove({
-        "ObjectId":"6383db0535d8b3d38d21ba39"
-        });
-    
-    });  
-    
-    
+router.delete("/borrarArticulo", (req, res) => {
+  db.articulos.remove({
+    ObjectId: "6383db0535d8b3d38d21ba39",
+  });
+});
 
-module.exports=router;
+module.exports = router;
