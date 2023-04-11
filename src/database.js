35334b98d81bc09342d897ae1518240ff092da70
JaredPV <jaredpv99@micorreo.upp.edu.mx>
2023-04-10 20:10:50 -0600

    Base de datos en Azure

--- src/database.js
+++ src/database.js
@@ -1,4 +1,6 @@
 const mongoose=require('mongoose');
+const fs = require('fs');
+const mysql = require('mysql');
 
 mongoose.connect('mongodb+srv://Admin:Prometeo2022@cluster0.kqo6b6w.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser:true
@@ -7,21 +9,22 @@ mongoose.connect('mongodb+srv://Admin:Prometeo2022@cluster0.kqo6b6w.mongodb.net/
     .then(db=> console.log('DB de Mongo conectada'))
     .catch(err=>console.error(err))
 
-    const mysql = require('mysql');
+    
     var connection = mysql.createConnection({
-        host : "localhost",
-        user : "admin",
+        host : "easypoa.mysql.database.azure.com",
+        user : "Administrador",
         password : "Prometeo2022",
         database : "EasyPOA",
         port: 3306,
-        multipleStatements : true
+        multipleStatements : true,
+        
      })
 
  connection.connect((err)=>{
     if(!err){
         console.log("Conectado a la base de datos de mysql")
     }else{
-        console.log("Conexión a la base de datos de mysql fallida")
+        console.log(err)
     }
  })
 
